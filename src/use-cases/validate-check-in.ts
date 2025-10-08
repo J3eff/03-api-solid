78f166862095b28ae40fd812248fb8b3a8f95b2f
J3eff <jeffersonvideo125@gmail.com>
2025-10-07 23:23:23 -0300

    feat: Implementando casos de uso - Validando horário do check-in

--- src/use-cases/validate-check-in.ts
+++ src/use-cases/validate-check-in.ts
@@ -1,6 +1,8 @@
 import { CheckIn } from '@prisma/client'
 import { CheckInsRepository } from '@/repositories/check-in-repository'
 import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import dayjs from 'dayjs'
+import { LateCheckInValidateError } from './errors/late-check-in-validate-error'
 
 interface ValidateCheckInUseCaseRequest {
   checkInId: string
@@ -20,6 +22,15 @@ export class ValidateCheckInUseCase {
 
     if (!checkIn) {
       throw new ResourceNotFoundError()
+    }
+
+    const distanceInMinutesFromCreation = dayjs(new Date()).diff(
+      dayjs(checkIn.created_at),
+      'minute',
+    )
+
+    if (distanceInMinutesFromCreation > 20) {
+      throw new LateCheckInValidateError()
     }
 
     checkIn.validated_at = new Date()
